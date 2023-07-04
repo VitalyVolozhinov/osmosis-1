57d727a4fc009076eff3801720358672a972bd8a
Vitaly Volozhinov <vitalyvolozhinov@gmail.com>
2023-07-04 10:15:57 +0200

    Fixing proto

--- x/poolmanager/client/queryproto/query.pb.gw.go
+++ x/poolmanager/client/queryproto/query.pb.gw.go
@@ -556,11 +556,11 @@ func local_request_Query_TotalPoolLiquidity_0(ctx context.Context, marshaler run
 }
 
 var (
-	filter_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0 = &utilities.DoubleArray{Encoding: map[string]int{"pool_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+	filter_Query_EstimateTradeBasedOnPriceImpact_0 = &utilities.DoubleArray{Encoding: map[string]int{"pool_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
-func request_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq EstimateTradeAmountInAmountOutBasedOnPriceImpactRequest
+func request_Query_EstimateTradeBasedOnPriceImpact_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq EstimateTradeBasedOnPriceImpactRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -584,17 +584,17 @@ func request_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(ctx contex
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EstimateTradeBasedOnPriceImpact_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.EstimateTradeAmountInAmountOutBasedOnPriceImpact(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.EstimateTradeBasedOnPriceImpact(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
 
-func local_request_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq EstimateTradeAmountInAmountOutBasedOnPriceImpactRequest
+func local_request_Query_EstimateTradeBasedOnPriceImpact_0(ctx context.Context, marshaler runtime.Marshaler, server QueryServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq EstimateTradeBasedOnPriceImpactRequest
 	var metadata runtime.ServerMetadata
 
 	var (
@@ -618,11 +618,11 @@ func local_request_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(ctx 
 	if err := req.ParseForm(); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_EstimateTradeBasedOnPriceImpact_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := server.EstimateTradeAmountInAmountOutBasedOnPriceImpact(ctx, &protoReq)
+	msg, err := server.EstimateTradeBasedOnPriceImpact(ctx, &protoReq)
 	return msg, metadata, err
 
 }
@@ -863,7 +863,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 
 	})
 
-	mux.Handle("GET", pattern_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_EstimateTradeBasedOnPriceImpact_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -874,7 +874,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := local_request_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(rctx, inboundMarshaler, server, req, pathParams)
+		resp, md, err := local_request_Query_EstimateTradeBasedOnPriceImpact_0(rctx, inboundMarshaler, server, req, pathParams)
 		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
@@ -882,7 +882,7 @@ func RegisterQueryHandlerServer(ctx context.Context, mux *runtime.ServeMux, serv
 			return
 		}
 
-		forward_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_EstimateTradeBasedOnPriceImpact_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1127,7 +1127,7 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 
 	})
 
-	mux.Handle("GET", pattern_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_Query_EstimateTradeBasedOnPriceImpact_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
@@ -1136,14 +1136,14 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_Query_EstimateTradeBasedOnPriceImpact_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_Query_EstimateTradeBasedOnPriceImpact_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -1171,7 +1171,7 @@ var (
 
 	pattern_Query_TotalPoolLiquidity_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 2, 5}, []string{"osmosis", "poolmanager", "v1beta1", "pools", "pool_id", "total_pool_liquidity"}, "", runtime.AssumeColonVerbOpt(false)))
 
-	pattern_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"osmosis", "poolmanager", "v1beta1", "pool_id", "estimate_trade"}, "", runtime.AssumeColonVerbOpt(false)))
+	pattern_Query_EstimateTradeBasedOnPriceImpact_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3, 2, 4}, []string{"osmosis", "poolmanager", "v1beta1", "pool_id", "estimate_trade"}, "", runtime.AssumeColonVerbOpt(false)))
 )
 
 var (
@@ -1195,5 +1195,5 @@ var (
 
 	forward_Query_TotalPoolLiquidity_0 = runtime.ForwardResponseMessage
 
-	forward_Query_EstimateTradeAmountInAmountOutBasedOnPriceImpact_0 = runtime.ForwardResponseMessage
+	forward_Query_EstimateTradeBasedOnPriceImpact_0 = runtime.ForwardResponseMessage
 )
