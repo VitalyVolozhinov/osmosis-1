2aa4bdafd5bdecd30af5218eadc06f928b8822d9
Osmosis <105710778+osmo-bot@users.noreply.github.com>
2023-07-28 08:35:01 +0200

    auto: update Go import paths to v17 on branch main (#5882)
    * auto: update Go import paths to v17
    
    * gen proto
    
    ---------
    
    Co-authored-by: czarcas7ic <czarcas7ic@users.noreply.github.com>
    Co-authored-by: Adam Tucker <adamleetucker@outlook.com>
    Co-authored-by: mattverse <mattpark1028@gmail.com>

--- x/poolmanager/client/cli/query_test.go
+++ x/poolmanager/client/cli/query_test.go
@@ -6,12 +6,12 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	"github.com/osmosis-labs/osmosis/v16/x/poolmanager/types"
+	"github.com/osmosis-labs/osmosis/v17/x/poolmanager/types"
 
 	"github.com/stretchr/testify/suite"
 
-	"github.com/osmosis-labs/osmosis/v16/app/apptesting"
-	poolmanagerqueryproto "github.com/osmosis-labs/osmosis/v16/x/poolmanager/client/queryproto"
+	"github.com/osmosis-labs/osmosis/v17/app/apptesting"
+	poolmanagerqueryproto "github.com/osmosis-labs/osmosis/v17/x/poolmanager/client/queryproto"
 )
 
 type QueryTestSuite struct {
